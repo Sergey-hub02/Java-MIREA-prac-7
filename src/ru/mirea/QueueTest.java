442f97dd8a7c380b96e9e9bae3c4c1ccd20058fa
Sergey-hub02 <s.park190802@gmail.com>
2021-10-01 11:52:21 +0300

    Задание доделано, и отчёт выполнен

---
+++ src/ru/mirea/QueueTest.java
@@ -0,0 +1,72 @@
+package ru.mirea;
+
+import java.util.Scanner;
+import java.util.LinkedList;
+
+public class QueueTest {
+  private static final int AMOUNT_OF_CARDS = 5;
+  private static final Scanner IN = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    var firstPlayerCards = new LinkedList<Integer>();
+    var secondPlayerCards = new LinkedList<Integer>();
+
+    System.out.print("Введите карты 1-го игрока: ");
+    for (int i = 0; i < AMOUNT_OF_CARDS; ++i) {
+      firstPlayerCards.add(IN.nextInt());
+    }
+
+    System.out.print("Введите карты 2-го игрока: ");
+    for (int i = 0; i < AMOUNT_OF_CARDS; ++i) {
+      secondPlayerCards.add(IN.nextInt());
+    }
+
+    int steps = 0;
+
+    while (true) {
+      if (steps >= 106) {
+        System.out.println();
+        System.out.println("botva");
+        return;
+      }
+
+      if (firstPlayerCards.isEmpty())
+        break;
+
+      int firstPlayerCard = firstPlayerCards.poll();
+
+      if (secondPlayerCards.isEmpty())
+        break;
+
+      int secondPlayerCard = secondPlayerCards.poll();
+
+      if (firstPlayerCard < secondPlayerCard) {
+        if (firstPlayerCard == 0 && secondPlayerCard == 9) {   // младшая карта побеждает старшую
+          firstPlayerCards.add(firstPlayerCard);
+          firstPlayerCards.add(secondPlayerCard);
+        }
+        else {
+          secondPlayerCards.add(firstPlayerCard);
+          secondPlayerCards.add(secondPlayerCard);
+        }
+      }
+      else {
+        if (firstPlayerCard == 9 && secondPlayerCard == 0) {    // младшая карта побеждает старшую
+          secondPlayerCards.add(firstPlayerCard);
+          secondPlayerCards.add(secondPlayerCard);
+        }
+        else {
+          firstPlayerCards.add(firstPlayerCard);
+          firstPlayerCards.add(secondPlayerCard);
+        }
+      }
+
+      ++steps;
+    }
+
+    String winner = (firstPlayerCards.isEmpty()) ? "second" : "first";
+
+    System.out.println();
+    System.out.println(winner + " " + steps);
+  }
+}
--- src/ru/mirea/StackTest.java
+++ src/ru/mirea/StackTest.java
@@ -1,24 +1,30 @@
 package ru.mirea;
 
+import java.util.Stack;
 import java.util.Scanner;
-import java.util.ArrayDeque;
+import java.util.Collections;
 
-public class Game {
+public class StackTest {
   private static final int AMOUNT_OF_CARDS = 5;
   private static final Scanner IN = new Scanner(System.in);
 
+  private static void reverse(Stack<Integer> s1, Stack<Integer> s2) {
+    Collections.reverse(s1);
+    Collections.reverse(s2);
+  }
+
   public static void main(String[] args) {
-    var firstPlayerCards = new ArrayDeque<Integer>();
-    var secondPlayerCards = new ArrayDeque<Integer>();
+    var firstPlayerCards = new Stack<Integer>();
+    var secondPlayerCards = new Stack<Integer>();
 
     System.out.print("Введите карты 1-го игрока: ");
     for (int i = 0; i < AMOUNT_OF_CARDS; ++i) {
-      firstPlayerCards.add(IN.nextInt());
+      firstPlayerCards.push(IN.nextInt());
     }
 
     System.out.print("Введите карты 2-го игрока: ");
     for (int i = 0; i < AMOUNT_OF_CARDS; ++i) {
-      secondPlayerCards.add(IN.nextInt());
+      secondPlayerCards.push(IN.nextInt());
     }
 
     int steps = 0;
@@ -30,34 +36,37 @@ public class Game {
         return;
       }
 
+      reverse(firstPlayerCards, secondPlayerCards);
+
       if (firstPlayerCards.isEmpty())
         break;
 
-      int firstPlayerCard = firstPlayerCards.poll();
+      int firstPlayerCard = firstPlayerCards.pop();
 
       if (secondPlayerCards.isEmpty())
         break;
 
-      int secondPlayerCard = secondPlayerCards.poll();
+      int secondPlayerCard = secondPlayerCards.pop();
 
+      reverse(firstPlayerCards, secondPlayerCards);
       if (firstPlayerCard < secondPlayerCard) {
         if (firstPlayerCard == 0 && secondPlayerCard == 9) {   // младшая карта побеждает старшую
-          firstPlayerCards.add(firstPlayerCard);
-          firstPlayerCards.add(secondPlayerCard);
+          firstPlayerCards.push(firstPlayerCard);
+          firstPlayerCards.push(secondPlayerCard);
         }
         else {
-          secondPlayerCards.add(firstPlayerCard);
-          secondPlayerCards.add(secondPlayerCard);
+          secondPlayerCards.push(firstPlayerCard);
+          secondPlayerCards.push(secondPlayerCard);
         }
       }
       else {
         if (firstPlayerCard == 9 && secondPlayerCard == 0) {    // младшая карта побеждает старшую
-          secondPlayerCards.add(firstPlayerCard);
-          secondPlayerCards.add(secondPlayerCard);
+          secondPlayerCards.push(firstPlayerCard);
+          secondPlayerCards.push(secondPlayerCard);
         }
         else {
-          firstPlayerCards.add(firstPlayerCard);
-          firstPlayerCards.add(secondPlayerCard);
+          firstPlayerCards.push(firstPlayerCard);
+          firstPlayerCards.push(secondPlayerCard);
         }
       }
 
